b8e0772c6a183458e8296398bf5a07db987631cb
terence-da-conceicao <158314737+terence-da-conceicao@users.noreply.github.com>
2024-10-07 16:46:48 +0200

    gestion des settings (coordination parametre URL user_id et ses infos)

--- Niveau1/settings.php
+++ Niveau1/settings.php
@@ -1,5 +1,12 @@
 <!doctype html>
 <html lang="fr">
+<?php 
+    if (isset($_GET['user_id'])) {
+        $userId = intval($_GET['user_id']);
+    } else {
+        echo "ID d'utilisatrice inconnu";
+    } ?>
+
     <head>
         <meta charset="utf-8">
         <title>ReSoC - Paramètres</title> 
@@ -11,16 +18,18 @@
             <img src="resoc.jpg" alt="Logo de notre réseau social"/>
             <nav id="menu">
                 <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
+                <a href="wall.php?user_id=<?php echo $userId ?>">Mur</a>
+                <a href="feed.php?user_id=<?php echo $userId ?>">Flux</a>
+                <!-- <a href="tags.php?tag_id= -->
+               <!-- et là on met le php userId etc echo amchin --> 
+                <!-- */ ">Mots-clés</a> -->
             </nav>
             <nav id="user">
                 <a href="#">Profil</a>
                 <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                    <li><a href="settings.php?user_id=<?php echo $userId ?>">Paramètres</a></li>
+                    <li><a href="followers.php?user_id=<?php echo $userId ?>">Mes suiveurs</a></li>
+                    <li><a href="subscriptions.php?user_id=<?php echo $userId ?>">Mes abonnements</a></li>
                 </ul>
 
             </nav>
@@ -46,12 +55,12 @@
                  * Documentation : https://www.php.net/manual/fr/reserved.variables.get.php
                  * ... mais en résumé c'est une manière de passer des informations à la page en ajoutant des choses dans l'url
                  */
-                $userId = intval($_GET['user_id']);
+                // $userId = intval($_GET['user_id']);
 
                 /**
                  * Etape 2: se connecter à la base de donnée
                  */
-                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                $mysqli = new mysqli("localhost", "root", "", "socialnetwork");
 
                 /**
                  * Etape 3: récupérer le nom de l'utilisateur
@@ -60,11 +69,11 @@
                     SELECT users.*, 
                     count(DISTINCT posts.id) as totalpost, 
                     count(DISTINCT given.post_id) as totalgiven, 
-                    count(DISTINCT recieved.user_id) as totalrecieved 
+                    count(DISTINCT received.user_id) as totalreceived 
                     FROM users 
                     LEFT JOIN posts ON posts.user_id=users.id 
                     LEFT JOIN likes as given ON given.user_id=users.id 
-                    LEFT JOIN likes as recieved ON recieved.post_id=posts.id 
+                    LEFT JOIN likes as received ON received.post_id=posts.id 
                     WHERE users.id = '$userId' 
                     GROUP BY users.id
                     ";
@@ -75,25 +84,27 @@
                 }
                 $user = $lesInformations->fetch_assoc();
 
+                
+
                 /**
                  * Etape 4: à vous de jouer
                  */
                 //@todo: afficher le résultat de la ligne ci dessous, remplacer les valeurs ci-après puiseffacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "</pre>";
+                //echo "<pre>" . print_r($userId, 1) . "</pre>";
                 ?>                
                 <article class='parameters'>
                     <h3>Mes paramètres</h3>
                     <dl>
                         <dt>Pseudo</dt>
-                        <dd>Félicie</dd>
+                        <dd><?php echo $user['alias'] ?></dd>
                         <dt>Email</dt>
-                        <dd>felicie@test.org</dd>
+                        <dd><?php echo $user['email'] ?></dd>
                         <dt>Nombre de message</dt>
-                        <dd>42</dd>
+                        <dd><?php echo $user['totalpost'] ?></dd>
                         <dt>Nombre de "J'aime" donnés </dt>
-                        <dd>12</dd>
+                        <dd><?php echo $user['totalgiven'] ?></dd>
                         <dt>Nombre de "J'aime" reçus</dt>
-                        <dd>53</dd>
+                        <dd><?php echo $user['totalreceived'] ?></dd>
                     </dl>
 
                 </article>
