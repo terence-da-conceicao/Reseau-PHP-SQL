2f2a6849b79d251c1a3ee9155df6afd98da868b6
terence-da-conceicao <158314737+terence-da-conceicao@users.noreply.github.com>
2024-10-07 15:23:16 +0200

    suppression dans news.php des trucs à cacher. gestiond 'admin.php : mots-clés et leurs id + association des messages, utilisatrices et leurs id + association du bon id

--- Niveau1/admin.php
+++ Niveau1/admin.php
@@ -31,7 +31,7 @@
          * Etape 1: Ouvrir une connexion avec la base de donnée.
          */
         // on va en avoir besoin pour la suite
-        $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+        $mysqli = new mysqli("localhost", "root", "", "socialnetwork");
         //verification
         if ($mysqli->connect_errno)
         {
@@ -61,13 +61,13 @@
                  */
                 while ($tag = $lesInformations->fetch_assoc())
                 {
-                    echo "<pre>" . print_r($tag, 1) . "</pre>";
+                    //echo "<pre>" . print_r($tag, 1) . "</pre>";
                     ?>
                     <article>
-                        <h3>#chaussette</h3>
-                        <p>id:321</p>
+                        <h3><?php echo $tag['label'] ?></h3>
+                        <p>id:<?php echo $tag['id'] ?> </p>
                         <nav>
-                            <a href="tags.php?tag_id=321">Messages</a>
+                            <a href="tags.php?tag_id=<?php echo $tag['id'] ?>">Messages</a>
                         </nav>
                     </article>
                 <?php } ?>
@@ -92,19 +92,19 @@
                  * Etape 5 : @todo : Afficher les utilisatrices en s'inspirant de ce qui a été fait dans news.php
                  * Attention à en pas oublier de modifier dans le lien les "user_id=123" avec l'id de l'utilisatrice
                  */
-                while ($tag = $lesInformations->fetch_assoc())
+                while ($users = $lesInformations->fetch_assoc())
                 {
-                    echo "<pre>" . print_r($tag, 1) . "</pre>";
+                    //echo "<pre>" . print_r($tag, 1) . "</pre>";
                     ?>
                     <article>
-                        <h3>Alexandra</h3>
-                        <p>id:123</p>
+                        <h3><?php echo $users['alias'] ?></h3>
+                        <p>id: <?php echo $users['id'] ?></p>
                         <nav>
-                            <a href="wall.php?user_id=123">Mur</a>
-                            | <a href="feed.php?user_id=123">Flux</a>
-                            | <a href="settings.php?user_id=123">Paramètres</a>
-                            | <a href="followers.php?user_id=123">Suiveurs</a>
-                            | <a href="subscriptions.php?user_id=123">Abonnements</a>
+                            <a href="wall.php?user_id=<?php echo $users['id'] ?>">Mur</a>
+                            | <a href="feed.php?user_id=<?php echo $users['id'] ?>">Flux</a>
+                            | <a href="settings.php?user_id=<?php echo $users['id'] ?>">Paramètres</a>
+                            | <a href="followers.php?user_id=<?php echo $users['id'] ?>">Suiveurs</a>
+                            | <a href="subscriptions.php?user_id=<?php echo $users['id'] ?>">Abonnements</a>
                         </nav>
                     </article>
                 <?php } ?>
