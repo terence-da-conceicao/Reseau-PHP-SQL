b637df9bf10bbf53d65053046bc6a5878d0de532
terence-da-conceicao <158314737+terence-da-conceicao@users.noreply.github.com>
2024-10-14 16:39:12 +0200

    ajout de messages d'erreurs en fonction du mdp et de l'email. Retrait temporaire du cryptage

--- Niveau1/login.php
+++ Niveau1/login.php
@@ -1,4 +1,4 @@
-<?php session_start(); ?>
+<!--<php session_start(); ?>"-->
 
 <!doctype html>
 <html lang="fr">
@@ -30,7 +30,7 @@
                             $emailAVerifier = $mysqli->real_escape_string($emailAVerifier);
                             $passwdAVerifier = $mysqli->real_escape_string($passwdAVerifier);
                             
-                            $passwdAVerifier = md5($passwdAVerifier);
+                            //$passwdAVerifier = md5($passwdAVerifier);
                             //tous les mdp sont "test" ,cryptés en 098f6bcd4621d373cade4e832627b4f6
                             //le mdp de tata est "bonjour", crypté en f02368945726d5fc2a14eb576f7276c0
 
@@ -43,25 +43,41 @@
 
                             $res = $mysqli->query($lInstructionSql);
                             $user = $res->fetch_assoc();
-                            
-                            if (!$user OR ($user['password']) != $passwdAVerifier) {
-                                echo "La connexion a échoué. ";
-                                echo "     bdd: ";
-                                echo
-                                print_r($user['password']);
-                                echo "    post: ";
-                                print_r($passwdAVerifier);
-
+                            $connexionPassword = false;
+                            $connexionEmail = false;
+                     
+                            if (empty($emailAVerifier)) {
+                                echo "Entrez un e-mail. ";
+                            } else if (!$user OR ($user['email']) != $emailAVerifier) {
+                                echo "Mauvais email. ";
                             } else {
-                                echo "Votre connexion est un succès : " . $user['alias'] . ".";
-                                // Etape 7 : Se souvenir que l'utilisateur s'est connecté pour la suite
-                                // documentation: https://www.php.net/manual/fr/session.examples.basic.php
-                                $_SESSION['connected_id']=$user['id'];
+                                $connexionEmail = true;
                             }
+
+
+                            if ($connexionEmail === true) { 
+                                if (empty($passwdAVerifier)) {
+                                    echo "Entrez un mot de passe. ";
+                                } else if (!$user OR ($user['password'])!=$passwdAVerifier) {
+                                    /*pour récupérer le mdp dans la bdd, il faut récupérer l'email.*/ 
+                                    /*Si le champ email n'est pas rempli, on dit juste de rentrer un mail */
+                                    echo "Mauvais mot de passe. ";
+                                } else {
+                                    $connexionPassword = true;
+                                }
+                            }
+
+                            if ($connexionEmail === true && $connexionPassword === true) {
+                                echo "Votre connexion est un succès, ".$user['alias'].".";
+                                /*$_SESSION['connected_id']=$user['id'];*/
+                            } else {
+                                echo "<br>Connexion échouée";
+                            }
+
                         }
                     ?>                     
                     <form action="login.php" method="post">
-                        <!--<input type='hidden'name='form_id' value='achanger'>-->
+                        <input type='hidden'name='form_id' value='achanger'>
                         <dl>
                             <dt><label for='email'>E-Mail</label></dt>
                             <dd><input type='email'name='email'></dd>
--- Niveau1/tags.php
+++ Niveau1/tags.php
@@ -54,8 +54,7 @@
                 $laQuestionEnSql = "
                     SELECT posts.content,
                     posts.created,
-                    users.alias as author_name,
-                    users.id,  
+                    users.alias as author_name,  
                     count(likes.id) as like_number,  
                     tags.label AS onetag,
                     GROUP_CONCAT(DISTINCT tags.label) AS taglist 
@@ -77,8 +76,6 @@
                     echo("Échec de la requete : " . $mysqli->error);
                 }
 
-                include 'generated_url.php';
-
                 // Etape 4: Parcourir les messsages et remplir le HTML
                 while ($post = $lesInformations->fetch_assoc())
                 {
@@ -88,9 +85,7 @@
                         <h3>
                             <time datetime='2020-02-01 11:12:13' ><?php echo $post['created'] ?></time>
                         </h3>
-                            <address> <a href="<?php echo $wallUrl ?>?user_id=<?php echo $post['id'] ?>">
-                            par <?php echo $post['author_name'] ?> </a>
-                            </address>
+                            <address>par <?php echo $post['author_name'] ?></address>
                         <div>
                             <p><?php echo $post['content'] ?></p>
                         </div>                                            
